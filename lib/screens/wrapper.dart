3f666ebbdd72649e77188a362964e64a9f51ea1a
Constab1e <constab1e@users.noreply.github.com>
2020-01-30 00:17:40 +0300

    added logout

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_with_firebase_training/screens/authenticate/authenticate.dart';
-import 'package:flutter_with_firebase_training/screens/home/home.dart';
+import 'package:flutter_with_firebase_training/models/user.dart';
+import 'home/home.dart';
+import 'package:provider/provider.dart';
+
 
 class Wrapper extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
 
+    final user = Provider.of<User>(context);
+    print(user);
+
+    
+
     //return eight home or authenticate widget
-    return Authenticate();
+    if (user == null){
+      return Authenticate();
+    }else{
+      return Home();
+    }
   }
 }
